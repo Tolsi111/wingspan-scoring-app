6e54c0943d7d20d2d7440972d4f161db8155140e
Romulus Andrei Bacila <30293758+Tolsi111@users.noreply.github.com>
2025-01-29 10:16:14 +0200

    General structure (#1)
    * generated components
    
    * base css
    
    * background image

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { CalculatorComponent } from './pages/calculator/calculator.component';
+import { AuthComponent } from './pages/auth/auth.component';
+import { StatisticsComponent } from './pages/statistics/statistics.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'calculator',
+        component: CalculatorComponent
+    },
+    {
+        path: 'auth',
+        component: AuthComponent
+    },
+    {
+        path: 'statistics',
+        component: StatisticsComponent
+    },
+    {
+        path: '**',
+        redirectTo: '/calculator',
+        pathMatch: 'full',
+      }
+];

c1bd5dfc972296cba537b5ff2084b61cbe4618e3
AndreiBacila <30293758+Tolsi111@users.noreply.github.com>
2025-07-22 16:00:08 +0300

    Refactored calculator system
    Made playerColumn component generic, in order to support scoring for any game

--- src/app/pages/calculator/player-column/player-column.component.ts
+++ src/app/pages/calculator/player-column/player-column.component.ts
@@ -1,73 +1,44 @@
-import { Component, computed, inject, input, OnDestroy, OnInit, Signal, signal } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-
-import { PlayerModel } from '../../../models/player.model';
-import { combineLatest, debounceTime, delay, delayWhen, Observable, Subscription } from 'rxjs';
-import { AsyncPipe } from '@angular/common';
+import {
+  Component,
+  DestroyRef,
+  inject,
+  input,
+  OnInit,
+  signal,
+} from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CalculatorService } from '../../../services/calculator.service';
 
 @Component({
   selector: 'app-player-column',
   imports: [ReactiveFormsModule],
   templateUrl: './player-column.component.html',
-  styleUrl: './player-column.component.scss'
+  styleUrl: './player-column.component.scss',
 })
-export class PlayerColumnComponent implements OnInit, OnDestroy {
+export class PlayerColumnComponent implements OnInit {
+  player = input.required<FormGroup>();
+  controlNames: string[] = [];
+  total = signal<number>(0);
 
-  form = signal<FormGroup>(new FormGroup({}));
-  private fb = inject(FormBuilder);
-  
-  name = signal<string>('');
-  birds = signal<number>(0);
-  bonusCards = signal<number>(0);
-  endOfRoundGoals = signal<number>(0);
-  eggs = signal<number>(0);
-  foodOnCards = signal<number>(0);
-  tuckedCards = signal<number>(0);
-  nectar = signal<number>(0);
-
-  totalScore = computed(() => {
-    return this.birds() + 
-    this.bonusCards() + 
-    this.endOfRoundGoals() + 
-    this.eggs() + 
-    this.foodOnCards() + 
-    this.tuckedCards() + 
-    this.nectar();
-  });
-
-  private formSubscriptions: Subscription[] = [];
+  private destroyRef = inject(DestroyRef);
+  private calculatorService = inject(CalculatorService);
 
   ngOnInit() {
-    this.initForm();
-    this.setupFormValueChange();
-  }
+    Object.keys(this.player().controls).forEach((key) => {
+      this.controlNames.push(key);
+    });
 
-  ngOnDestroy() {
-    this.formSubscriptions.forEach(sub => sub.unsubscribe());
-  }
+    // Add option to keep total score hidden?
+    const totalScoreSub = this.player()
+      .valueChanges // .pipe(debounceTime(500))
+      .subscribe(() => {
+        this.total.set(
+          this.calculatorService.getPlayerTotalScore(this.player())
+        );
+      });
 
-  initForm() {
-    this.form().addControl('name', this.fb.control('', Validators.required));
-    this.form().addControl('birds', this.fb.control(0, Validators.required));
-    this.form().addControl('bonusCards', this.fb.control(0, Validators.required));
-    this.form().addControl('endOfRoundGoals', this.fb.control(0, Validators.required));
-    this.form().addControl('eggs', this.fb.control(0, Validators.required));
-    this.form().addControl('foodOnCards', this.fb.control(0, Validators.required));
-    this.form().addControl('tuckedCards', this.fb.control(0, Validators.required));
-    this.form().addControl('nectar', this.fb.control(0, Validators.required));
+    this.destroyRef.onDestroy(() => {
+      totalScoreSub.unsubscribe();
+    });
   }
-
-  setupFormValueChange() {
-    // .pipe(debounceTime(500))
-    this.formSubscriptions.push(
-      this.form().get('birds')!.valueChanges.subscribe((value) => { this.birds.set(value) }),
-      this.form().get('bonusCards')!.valueChanges.subscribe((value) => { this.bonusCards.set(value) }),
-      this.form().get('endOfRoundGoals')!.valueChanges.subscribe((value) => { this.endOfRoundGoals.set(value) }),
-      this.form().get('eggs')!.valueChanges.subscribe((value) => { this.eggs.set(value) }),
-      this.form().get('foodOnCards')!.valueChanges.subscribe((value) => { this.foodOnCards.set(value) }),
-      this.form().get('tuckedCards')!.valueChanges.subscribe((value) => { this.tuckedCards.set(value) }),
-      this.form().get('nectar')!.valueChanges.subscribe((value) => { this.nectar.set(value) })
-    )
-  }
-
 }
